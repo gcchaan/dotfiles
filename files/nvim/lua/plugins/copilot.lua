518f83c902e1cc84a56601a786494439efd17999
gcchaan <gcchaan@users.noreply.github.com>
2025-07-05 21:20:58 +0900

    Specify node path for executing copilot

---
+++ files/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,6 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/24.0.1/bin/node", -- Node.js version must be > 20
+  }
+}
