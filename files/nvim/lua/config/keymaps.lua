a54d3299bac28bf2bfb1edd0b50afa568ced4a78
gcchaan <gcchaan@users.noreply.github.com>
2025-07-15 15:25:05 +0900

    feat: add lazydocker keymap and zsh abbreviation
    - Added a Neovim keymap for launching lazydocker via Snacks.terminal if
      lazydocker is available on the system.
    - Introduced a zsh abbreviation `ld` for running lazydocker quickly.
    - Updated keymap and abbreviation files to use consistent quote styles.

--- files/nvim/lua/config/keymaps.lua
+++ files/nvim/lua/config/keymaps.lua
@@ -1,20 +1,26 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { noremap = true })
-vim.keymap.set('n', '<S-h>', '^', opts)
-vim.keymap.set('n', '<S-l>', '$', opts)
-vim.keymap.set('n', '<S-j>', '}', opts)
-vim.keymap.set('n', '<S-k>', '{', opts)
+vim.keymap.set({ "n", "v" }, "j", "gj", { noremap = true })
+vim.keymap.set({ "n", "v" }, "k", "gk", { noremap = true })
+vim.keymap.set("n", "<S-h>", "^", opts)
+vim.keymap.set("n", "<S-l>", "$", opts)
+vim.keymap.set("n", "<S-j>", "}", opts)
+vim.keymap.set("n", "<S-k>", "{", opts)
 
-vim.keymap.set('n', 'x', '"_x', opts)
-vim.keymap.set('n', 's', '"_s', { noremap = true })
+vim.keymap.set("n", "x", '"_x', opts)
+vim.keymap.set("n", "s", '"_s', { noremap = true })
 
-vim.keymap.set('i', '{}', '{}<Left>', opts)
-vim.keymap.set('i', '[]', '[]<Left>', opts)
-vim.keymap.set('i', '""', '""<Left>', opts)
-vim.keymap.set('i', "''", "''<Left>", opts)
-vim.keymap.set('i', '<>', '<><Left>', opts)
+vim.keymap.set("i", "{}", "{}<Left>", opts)
+vim.keymap.set("i", "[]", "[]<Left>", opts)
+vim.keymap.set("i", '""', '""<Left>', opts)
+vim.keymap.set("i", "''", "''<Left>", opts)
+vim.keymap.set("i", "<>", "<><Left>", opts)
 
-vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Lazy Manager' })
-vim.keymap.set('n', '<leader><leader>', '<cmd>Lazy sync<CR>', { desc = 'Lazy Sync' })
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Lazy Manager" })
+vim.keymap.set("n", "<leader><leader>", "<cmd>Lazy sync<CR>", { desc = "Lazy Sync" })
+
+if vim.fn.executable("lazydocker") == 1 then
+  vim.keymap.set("n", "<leader>dd", function()
+    Snacks.terminal("lazydocker", { esc_esc = false, ctrl_hjkl = false })
+  end, { desc = "Lazydocker" })
+end
