222a94ae7e50446191e2843ee9a1b5c75e873417
gcchaan <gcchaan@users.noreply.github.com>
2026-05-02 13:18:41 +0900

    feat(nvim): include alpha in <C-a>/<C-x> nrformats
    Added "alpha" to vim.opt.nrformats to allow <C-a> and <C-x> to increment
    and decrement alphabetical characters as well as numbers. This enhances
    editing capabilities for buffers containing alphabetical sequences.

--- files/nvim/lua/config/options.lua
+++ files/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+
+-- Alphabetical characters should also be included in the <C-a> and <C-x> targets
+vim.opt.nrformats:append("alpha")
